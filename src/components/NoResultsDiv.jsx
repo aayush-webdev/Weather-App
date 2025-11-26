0c3c82f80f9984d0b75c84041b32af61ce866a81
Aayush sharma <aayush.webdevs@gmail.com>
2025-11-26 20:11:44 +0530

    Fix image source path in NoResultsDiv component

--- src/components/NoResultsDiv.jsx
+++ src/components/NoResultsDiv.jsx
@@ -3,7 +3,7 @@
 const NoResultsDiv = () => {
   return (
     <div className="no-results">
-        <img src="public/icons/no-result.svg" alt="No Results Found" className="icon" />
+        <img src="/icons/no-result.svg" alt="No Results Found" className="icon" />
         <h3 className="title">Something Went Wrong</h3>
         <p className="message"> We couldn't find any results for your search. Ensure you entered a valid city name. Or Try Again Later.</p>
       
