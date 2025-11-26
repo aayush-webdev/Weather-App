a7f64df6f8feb9cfeee83c63005b0c529ebceac4
Aayush sharma <aayush.webdevs@gmail.com>
2025-11-26 20:08:50 +0530

    Fix image source path for weather icon

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,7 +1,7 @@
 const CurrentWeather = ({ currentWeather }) => {
   return (
      <div className="current-weather">
-          <img src={`icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" />
+<img src={`/icons/${currentWeather.weatherIcon}.svg`} className="weather-icon" />
           <h2 className="temperature">{currentWeather.tempreture} <span>°C</span></h2>
           <p className="discription">{currentWeather.description}</p>
         </div>
